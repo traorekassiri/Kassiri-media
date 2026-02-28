473bf85e71ef0e89e975d59447912692a595ac1b
traorekassiri <traorekassiri@gmail.com>
2026-02-28 01:14:56 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ module.exports = {
         secondary: '#ff8c42',
         accent: '#e6b800',
       },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in-out',
+        'slide-up': 'slideUp 0.5s ease-out',
+        'slide-down': 'slideDown 0.5s ease-out',
+        'slide-left': 'slideLeft 0.5s ease-out',
+        'slide-right': 'slideRight 0.5s ease-out',
+      },
     },
   },
   plugins: [],
